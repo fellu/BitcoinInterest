2f9a05094acf111e1d1974702ad5e0a92bac97b6
Felix Ippolitov <fellu@f-solutions.fi>
2018-04-17 02:15:35 +0300

    started bitcore function implementations

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -128,6 +128,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "setnetworkactive", 0, "state" },
     { "getmempoolancestors", 1, "verbose" },
     { "getmempooldescendants", 1, "verbose" },
+    { "getaddressmempool", 0, "addresses"},
     { "bumpfee", 1, "options" },
     { "logging", 0, "include" },
     { "logging", 1, "exclude" },
